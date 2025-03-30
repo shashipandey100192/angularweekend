9f0e44d60ec91ec952c140413719c031c4446a9e
shashiweb <shashiweb64@gmail.com>
2025-03-30 15:43:19 +0530

    repo create

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AboutComponent } from './about/about.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,AboutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
