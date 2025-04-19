4775b7b7e8f9f3a4df03e3e6a43c0102fdc87ed3
shashiweb <shashiweb64@gmail.com>
2025-04-19 15:50:48 +0530

    newbranch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { AboutComponent } from './about/about.component';
-import { ContactComponent } from './contact/contact.component';
+
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,AboutComponent,ContactComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
